b2225eeb48f117dd3460a1e7d96d8f5cfa9d84fa
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-03 23:32:53 +0000

    Updating puts program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - The starter funtion for my code
+ *
+ * Description: This is a basic print string function
+ * Return: Return value is 0
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
