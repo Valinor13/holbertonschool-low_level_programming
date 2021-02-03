9dcaa15c7685bd4813e71cbbd55324f38c9a42c4
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-03 23:51:46 +0000

    Adding the printf program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the primary function of this program
+ *
+ * Description: its doing stuff
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
