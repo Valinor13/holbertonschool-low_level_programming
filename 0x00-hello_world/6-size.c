11384f8b1257de0eea2935884fde8e874fd5c346
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-04 00:13:55 +0000

    Adding size program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the primary function of this program
+ *
+ * Description: its doing stuff
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%d\n %d\n %d\n %d\n %d\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+	return (0);
+}
+
