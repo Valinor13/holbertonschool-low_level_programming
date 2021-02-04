f7c6988bec88afe93694c254b142b08ce76bebe8
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-04 00:41:44 +0000

    Updating the size of code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,11 @@
  */
 int main(void)
 {
-	printf("%d\n", "%d\n", "%d\n", "%d\n", "%d\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %d\n", sizeof(long int));
+	printf("Size of a long long int: %d\n", sizeof(long long int));
+	printf("Size of a float: %d\n", sizeof(float));
 	return (0);
 }
 
