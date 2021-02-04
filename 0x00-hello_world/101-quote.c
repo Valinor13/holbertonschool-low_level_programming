a0622c8857fb1bfe2211d829d59179fb4da7b26b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-04 04:12:34 +0000

    Adding quote program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+/**
+ * main - The initial main function
+ *
+ * Description: Using the write function to print stderr
+ * Return: returning 1
+ */
+int main(void)
+{
+	int fd[2];
+	char buf1[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(fd[2], buf1, strlen(buf1));
+	return (1);
+}
