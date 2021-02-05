e8c510d19b05029bad96b570eb3bf44421a54488
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-05 04:06:17 +0000

    Adding print numbers code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Main function comments
+ *
+ * Return: An integer value
+ */
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 11; base10++)
+		printf("%d\n", base10);
+return (0);
+}
