cd332a3be1a63c3126f8e226d018f7f91f9e547b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-05 04:38:20 +0000

    Adding print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main function comments
+ *
+ * Return: An integer value
+ */
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 10; base10++)
+		putchar(base10);
+	putchar('\n');
+return (0);
+}
+
