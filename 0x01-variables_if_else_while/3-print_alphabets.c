1b03262ddc1e08e3ddfdaf0c8745865dd8a7f726
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-05 02:25:25 +0000

    Adding the abc ABC program

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Random int designation
+ *
+ * Return: Value of None
+ */
+int  main(void)
+{
+	char abc;
+	char ABC;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+	{
+	putchar(abc);
+	}
+
+	for (ABC = 'A'; ABC <= 'Z'; ABC++)
+	{
+	putchar(ABC);
+	}
+return (0);
+}
+
+
