e0133103a79fb3b794835a0f2adef15a58d78670
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-05 05:17:39 +0000

    Adding mirror program

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+/**
+ * main - Random int designation
+ *
+ * Return: Value of None
+ */
+int  main(void)
+{
+	char abc;
+
+	for (abc = 'z'; abc >= 'a'; abc--)
+	{
+	putchar(abc);
+	}
+
+	putchar('\n');
+return (0);
+}
+
+
