cb1db17f91bce4826cd5c4c4779d80c4a7621cfe
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-06 04:03:04 +0000

    Updating print comb"

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,15 @@ int main(void)
 {
 	int num1;
 
-	for(num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 10; num1++)
 	{
 		putchar(num1 + '0');
-		putchar(',');
-		putchar(' ');
+
+		if (num1 < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('$');
