b7127288768478a5798d235eeb3df5546fe54c96
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-09 03:44:46 +0000

    Adding readme file and print Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * main - The main will print holberton
+ *
+ * Return: Return 1 for failure 0 for success
+ */
+int main(void)
+{
+	char arr[9];
+	int i;
+
+	arr[0] = 'H';
+	arr[1] = 'o';
+	arr[2] = 'l';
+	arr[3] = 'b';
+	arr[4] = 'e';
+	arr[5] = 'r';
+	arr[6] = 't';
+	arr[7] = 'o';
+	arr[8] = 'n';
+
+	i = 0;
+
+	while (i < 10)
+	{
+		_putchar(arr[i]);
+		i++;
+	}
+
+	_putchar('\n');
+
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the functions nested loops directory in the Holberton low level programming repository.
