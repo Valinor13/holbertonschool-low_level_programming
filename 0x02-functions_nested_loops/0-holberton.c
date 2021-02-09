25d728bb989fd928e5e0a7ad6afb5ad555bcb439
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-09 17:20:41 +0000

    Updating holberton print file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,24 +7,15 @@
  */
 int main(void)
 {
-	char arr[9];
-	int i;
-
-	arr[0] = 'H';
-	arr[1] = 'o';
-	arr[2] = 'l';
-	arr[3] = 'b';
-	arr[4] = 'e';
-	arr[5] = 'r';
-	arr[6] = 't';
-	arr[7] = 'o';
-	arr[8] = 'n';
-
-	for (i = 0; i < 10; i++)
-	{
-		_putchar(arr[i]);
-	}
-
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 return (0);
