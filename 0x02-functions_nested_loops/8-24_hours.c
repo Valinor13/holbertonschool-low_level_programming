e239a3edea53f3707c73d805f49f00eace51d1f4
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-10 02:51:23 +0000

    Adding 24 hours function

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - returns every minute of the day now
+ *
+ * Return: returns a print statement or 6
+ */
+void jack_bauer(void)
+{
+	int hrs, mins;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+return;
+}
