e31533f0d75c8f8511eb721ae97d51f4d6c78bd6
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-10 03:48:27 +0000

    Adding add function and updating header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - add funtion will add 2 integers
+ * @a: A is the first variable
+ * @b: b is the 2nd variable
+ * Return: returns the added value of the integers
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
