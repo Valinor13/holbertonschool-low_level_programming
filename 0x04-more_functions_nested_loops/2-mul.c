bcb5e0e3a9556a3f7d9d5652fc58060ba575f344
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-11 19:58:48 +0000

    Adding the multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies 2 numbers together
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: returns the value of the 2 numbers multiplied
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
