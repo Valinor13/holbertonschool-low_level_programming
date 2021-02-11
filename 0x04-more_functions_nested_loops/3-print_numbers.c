4476ac569ee92421ee5712ac1e47314767f39c3c
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-11 20:06:49 +0000

    Adding the print 0-9 funtion

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - printing a line of numbers
+ *
+ * Return: return is void
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+
+return;
+}
