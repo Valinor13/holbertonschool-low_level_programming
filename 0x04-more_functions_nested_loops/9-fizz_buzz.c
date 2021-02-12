e641ef1bc52ce70ccd8aefb007fc85d1bb1d8dd9
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-12 02:08:37 +0000

    Adding fizzbuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100 replacing 3 and 5s with fizzbuzz
+ *
+ * Return: Always returns 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+
+	putchar('\n');
+
+return (0);
+}
