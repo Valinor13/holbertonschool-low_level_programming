f74e141bd6a0186f471cf1181635c6d847c9ab94
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-12 02:59:26 +0000

    Adding print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - printing right triangles
+ * @size: inputs the size of the triangle
+ *
+ * Return: returns void
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	for (x = 0; x < size; x++)
+	{
+
+		for (y = x + 1; y < size; y++)
+		{
+			_putchar(' ');
+		}
+
+		for (z = size - x; z <= size; z++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+return;
+}
