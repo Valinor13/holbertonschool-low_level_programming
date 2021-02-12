ddbbe6717ed0f9d3750d48ecae4129abc9574c61
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-12 00:33:29 +0000

    Adding more numbers funtion

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,4 +5,22 @@
  *
  * Return: returns void
  */
+void more_numbers(void)
+{
+	int x;
+	int y;
 
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if ( y >= 10)
+			{
+				_putchar(49);
+			}
+			_putchar(y % 10 + '0');
+		}
+		_putchar('\n');
+	}
+return;
+}
