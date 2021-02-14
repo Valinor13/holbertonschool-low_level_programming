08378e40aea79ca4d8b437bb658a7a8473fbc7f2
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-14 19:24:59 +0000

    Updating print combos

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,17 +8,18 @@
 int main(void)
 {
 	int num1;
-	int num2;
 
-	num2 = 0;
-
-	for (num1 = 0; num2 == 9; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		putchar(num1 + '0');
-		putchar(num2 + '0');
-		putchar(',');
-		putchar(' ');
-		num2++;
+		putchar(num1 / 10 + '0');
+		putchar(num1 % 10 + '0');
+
+		if (num1 != 99)
+		{
+			putchar(44);
+			putchar(32);
+		}
 	}
+putchar('\n');
 return (0);
 }
