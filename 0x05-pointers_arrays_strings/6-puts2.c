fc0cd816244a9bfc110ef81de34902cff08e07e1
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-16 23:20:25 +0000

    updating puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,34 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i, len1;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	len1 = _strlen(str);
+
+	for (i = 0; i <= len1; i += 2)
 	{
 		_putchar(str[i]);
 	}
 	_putchar('\n');
 return;
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: input string
+ *
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+	len++;
+	}
+
+return (len);
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements of an array
+ * @a: the array
+ * @n: the number of elements to be printed
+ *
+ * Return: returns void
+ */
+void print_array(int *a, int n) 
+{
+
