cff0c397f79f4d93b2b57b6b8005c1b31b05dc0b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-16 22:07:28 +0000

    Updating print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -30,14 +30,14 @@ return;
  */
 int _strlen(char *s)
 {
-        int len;
+	int len;
 
-        len = 0;
+	len = 0;
 
-        while (s[len] != '\0')
-        {
-                len++;
-        }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
 
 return (len);
 }
