a5f9e78154eca81b16daa8f3795b966b09174c4f
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-19 06:03:54 +0000

    updatin rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -31,3 +31,24 @@ char *rot13(char *p)
 		}
 return (p);
 }
+
+
+/**
+ * _strlen - return the length of a string
+ * @s: input string
+ *
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+	len++;
+}
+
+return (len);
+}
+
