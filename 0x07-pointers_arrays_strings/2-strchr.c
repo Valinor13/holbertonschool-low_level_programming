9396db9027cd03ef9e5b8e3c72b8bd1189872131
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-22 21:59:33 +0000

    Adding strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * *_strchr - locates a char in a str
+ * @s: input string
+ * @c: input character being searched for
+ * Return: pointer or null char
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *r = "NULL";
+
+	for (i = 0; s[i] != 00; i++)
+	{
+		if (s[i] == c)
+		{
+			break;
+		}
+
+	}
+
+	if (s[i] == 00)
+	{
+		return (r);
+	}
+
+	else
+	{
+		return (&s[i]);
+	}
+}
