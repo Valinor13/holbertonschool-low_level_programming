c348db11721f4b703cb1482011cb145ffc3236e8
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-23 05:35:21 +0000

    Adding set string function

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: input pointer
+ * @to: input pointer with char
+ * Return: returns void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+return;
+}
