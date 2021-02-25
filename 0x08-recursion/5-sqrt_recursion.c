2bae17077a043b35638282c9694eb0c7cfffeb9b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-25 03:05:44 +0000

    Updating square root recursion for betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - finds the natural square root of an integer
- * @n: integer being passed
+ * my_func - finds the natural square root of an integer
+ * @n: integer being passed by square root function
+ * @x: incrementor of the square root
  * Return: returns natural square root or -1 for failure
  */
 
@@ -33,6 +34,13 @@ int my_func(int n, int x)
 		return (my_func(n, x + 1));
 	}
 }
+
+/**
+ * _sqrt_recursion - finds the square root of an integer
+ * @n: integer being passed by main
+ * Return: returns the square root to main
+ */
+
 int _sqrt_recursion(int n)
 {
 	return (my_func(n, 1));
