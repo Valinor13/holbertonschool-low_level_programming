f598a53494a0edc6ae6d6b17a704c3896dfe815b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-02-27 22:23:46 +0000

    Updated change maker

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,59 +17,33 @@ int main(int argc, char *argv[])
 		puts("Error");
 		return (1);
 	}
- 
 	s = argv[1];
-
 	while (*s != 00)
 	{
 		if (*s >= 48 && *s <= 57)
-		{
 			s++;
-		}
-
 		else
 		{
 			puts("Error");
 			return (1);
 		}
 	}
-
 	x = atoi(argv[1]);
-
 	if (x < 0)
 	{
 		puts("0");
 		return (0);
 	}
-
-	while (x != 0)
-	{
-		for (qtr = 0; x - 25 >= 0; qtr++)
-		{
-			x -= 25;
-		}
-
-		for (dime = 0; x - 10 >= 0; dime++)
-		{
-			x -= 10;
-		}
-
-		for (nik = 0; x - 5 >= 0; nik++)
-		{
-			x -= 5;
-		}
-
-		for (tobit = 0; x - 2 >= 0; tobit++)
-		{
-			x -= 2;
-		}
-
-		for (peni = 0; x - 1 >= 0; peni++)
-		{
-			x -= 1;
-		}
-	}
-
+	for (qtr = 0; x - 25 >= 0; qtr++)
+		x -= 25;
+	for (dime = 0; x - 10 >= 0; dime++)
+		x -= 10;
+	for (nik = 0; x - 5 >= 0; nik++)
+		x -= 5;
+	for (tobit = 0; x - 2 >= 0; tobit++)
+		x -= 2;
+	for (peni = 0; x - 1 >= 0; peni++)
+		x -= 1;
 	change = qtr + dime + nik + tobit + peni;
 	printf("%d\n", change);
 	return (0);
