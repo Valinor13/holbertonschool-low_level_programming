cd6c0fa2c5bcc6fce55cb1a1fa2db77f9260d54a
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-02 21:07:03 +0000

    Adding strdup function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string into the heap
+ * @str: input string
+ * Return: returns a pointer or NULL
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i, x;
+
+	x = 1;
+
+	for (i = 0; str[i] != 00; i++)
+	{
+		x++;
+	}
+
+	s = malloc(i * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != 00; i++)
+	{
+		s[i] = str[i];
+	}
+
+return (s);
+}
