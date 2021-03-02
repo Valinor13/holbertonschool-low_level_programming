d48040764071f6413a21451acb806d4f56fc5783
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-02 22:34:06 +0000

    Adding the string concatenate function

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function concatenates two strings
+ * @s1: alpha string
+ * @s2: omega string
+ * Return: returns a pointer
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int i, x, j, z, y;
+
+	i = 0;
+	x = 0;
+
+	while (s1[i] != 00)
+	{
+		i++;
+	}
+
+	while (s2[x] != 00)
+	{
+		x++;
+	}
+
+	z = i + x + 1;
+
+	s = malloc(z * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		s[j] = s1[j];
+	}
+
+	for (y = 0; y <= x; j++, y++)
+	{
+		s[j] = s2[y];
+	}
+
+return (s);
+}
