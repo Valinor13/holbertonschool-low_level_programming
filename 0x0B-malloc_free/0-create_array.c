2ea4e0db8cc878c57235982ec728a0523fa233bb
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-02 20:22:18 +0000

    First commit. Readme file and create dynamic array function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * create_array - creating an array with with a variable size
+ * @size: input size of array
+ * @c: input character to fill array
+ * Return: returns pointer or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		p[i] = c;
+	}
+
+return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is a readme file for the malloc and free directory in my Holberton low level programming repository.
