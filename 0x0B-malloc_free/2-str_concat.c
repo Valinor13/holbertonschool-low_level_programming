b6d1d434e4b0c33f35b57815ef563d8192498003
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-02 22:50:29 +0000

    Updating string concatenate to remove {}

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,43 +16,29 @@ char *str_concat(char *s1, char *s2)
 	x = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[i] != 00)
-	{
 		i++;
-	}
 
 	while (s2[x] != 00)
-	{
 		x++;
-	}
 
 	z = i + x + 1;
 
 	s = malloc(z * sizeof(char));
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	for (j = 0; j < i; j++)
-	{
 		s[j] = s1[j];
-	}
 
 	for (y = 0; y <= x; j++, y++)
-	{
 		s[j] = s2[y];
-	}
 
 return (s);
 }
