347a487c4d23139e8ef8f5c97c502e3461ece156
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-03 01:16:19 +0000

    Adding alloc grid function

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2 dimensional array of integers
+ * @width: the first integer
+ * @height: the second integer
+ * Return: rturns a table of x by y size or null
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j, len;
+	int *x, **y;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	len = sizeof(int *) * height + sizeof (int) * width * height;
+
+	y = (int **)malloc(len);
+
+	x = (int *)(y + height);
+
+	for (i = 0; i < height; i++)
+	{
+		y[i] = (x + width * i);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			y[i][j] = 0;
+		}
+	}
+
+return (y);
+}
