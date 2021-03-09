72b6ba9531628cfb95f08b52f252b2db4c89b37a
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-09 05:08:10 +0000

    Changing format of print dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,20 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			puts("Name: nil");
 
-	if (d->name == NULL)
-		puts("Name: nil");
+		else
+			printf("Name: %s\n", d->name);
 
-	else
-		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
 
-	printf("Age: %f\n", d->age);
+		if (d->owner == NULL)
+			puts("Owner: nil");
 
-	if (d->owner == NULL)
-		puts("Owner: nil");
-
-	else
-		printf("Owner: %s\n", d->owner);
+		else
+	}		printf("Owner: %s\n", d->owner);
 return;
 }
