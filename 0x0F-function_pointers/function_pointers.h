c77f51f33349a5d3c9df99300069c443d1ffa785
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-10 18:20:43 +0000

    updating include guards

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-
+#include <stdlib.h>
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 
 #endif
