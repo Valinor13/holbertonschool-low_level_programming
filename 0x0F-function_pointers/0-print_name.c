ffca921a3f369b5f4479846cef1265058a8bd515
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-10 17:46:25 +0000

    Adding the readme file and print name function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - calls functions to print
+ * @name: input string
+ * @f: input function
+ * Return: returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+	{
+		f(name);
+	}
+return;
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+In this function pointers directory I will have a series of function pointers solving basic tasks for Holberton.
