df64c885d969193c3877a00f58148fcb14abe281
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-11 18:31:09 +0000

    Adding readme file and sum fun

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums all input numbers
+ * @n: input count
+ * Return: returns sum or 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+	va_list args;
+
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i <= n; i++)
+	{
+		sum = sum + va_arg(args, int);
+	}
+
+return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This readme file is for the variadic functions directory in the Holberton low level programming repository.
