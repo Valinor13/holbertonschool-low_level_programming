f0d9bee27707d3c95c67720c4d22e90e0d8332f2
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-12 23:08:41 +0000

    Finishing print all ints function

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -8,29 +8,28 @@
  */
 void print_number(int n)
 {
-	unsigned int x, y, z;
-
-	y = n;
-	x = 1;
+	char c = '0';
 
 	if (n < 0)
 	{
-		_putchar('-');
-		y = n * -1;
+		if (n % 10 == 0)
+		{
+			_putchar('-');
+			n = (n * -1);
+		}
+
+		else
+		{
+			_putchar('-');
+			n++;
+			c = '1';
+			n = (n * -1);
+		}
 	}
 
-	while (x * 10 <= y)
-	{
-		x = x * 10;
-	}
+	if (n / 10 != 0)
+		print_number(n / 10);
 
-	while (x != 0)
-	{
-		z = y / x;
-		_putchar(z + '0');
-		y = y - z * x;
-		x = x / 10;
-	}
-
+	_putchar(n % 10 + c);
 return;
 }
