07b99e0f97eb6316ae79ae93049ff06168b51a79
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-23 19:12:53 +0000

    Adding the free2 func

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list but leaves the head null
+ * @head: input pointer to head of list
+ * Return: returns void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *noodle, *pasta;
+
+	pasta = *head;
+
+	while (pasta != NULL)
+	{
+		noodle = pasta->next;
+		free(pasta);
+		pasta = noodle;
+	}
+	head = NULL;
+return;
+}
