d4b05abc372b75b06a323b5431294ecdaa3f323d
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-23 20:51:08 +0000

    Adding pop head of list func

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - frees head of list
+ * @head: pointer to head of list
+ * Return: returns value stored in head
+ */
+int pop_listint(listint_t **head)
+{
+	int x;
+	listint_t *pasta;
+
+	if (head == NULL)
+		return (0);
+
+	pasta = *head;
+	x = pasta->n;
+	*head = pasta->next;
+	free(pasta);
+return (x);
+}
