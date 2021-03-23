9d65ba106dc5822042c2b1cffb2d8dd3b464ec13
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-23 18:25:34 +0000

    Adding the first free funciton

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees allocated memory in a llist
+ * @head: input pointer to head of list
+ * Return: returns void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *noodle;
+
+	while (head != NULL)
+	{
+		noodle = head->next;
+		head = noodle;
+		free(head);
+	}
+return;
+}
