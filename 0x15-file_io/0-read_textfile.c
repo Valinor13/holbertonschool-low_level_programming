1665a1d624a14e86c5bed311cd7402d052b6ca62
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-30 20:30:16 +0000

    Adding the file io directory with a readme and read_textfile function

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads an input .txt
+ * @filename: pointer to input .txt
+ * @letters: size of buffer for file r/w
+ * Return: returns signed int of chars written
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+	buff = malloc(sizeof(buff) * letters);
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	if (rd > 0)
+		wr = write(1, buff, rd);
+	if (wr == -1 || wr < rd)
+	{
+		free(buff);
+		return (0);
+	}
+
+	wr = close(fd);
+	free(buff);
+return (rd);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a new .txt file
+ * @filename: string for name of .txt
+ * @text_contents - what to put in the file
+ * Return: returns 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	
