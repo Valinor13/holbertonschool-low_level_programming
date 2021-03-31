8ab395e103bac77150b3ad535d9064ffccd0e9c7
Jay Calhoun <jwcalhoun2@gmail.com>
2021-03-31 15:24:04 +0000

    Adding the append a file function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,11 +23,8 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[bsz])
 		bsz++;
 	wr = write(fd, text_content, bsz);
-	if (wr == -1)
-	{
-		close(fd);
-		return (-1);
-	}
 	close(fd);
+	if (wr == -1)
+		return (-1);
 return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,4 +8,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	
+	int fd, bsz = 0, wr;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content[bsz])
+		bsz++;
+	wr = write(fd, text_content, bsz);
+	if (wr == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+return (1);
+}
