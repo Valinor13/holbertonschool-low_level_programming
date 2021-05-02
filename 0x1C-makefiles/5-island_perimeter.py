958b53474e0c2b16ea76b5855203ac821c32240c
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-02 22:40:30 +0000

    Updating module logic

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,8 +8,8 @@ def island_perimeter(grid):
     if not grid:
         return 0
     perim = 0
-    for x in range(1, len(grid) - 1):
-        for y in range(1, len(grid[x]) - 1):
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 if grid[x - 1][y] == 0:
                     perim += 1
