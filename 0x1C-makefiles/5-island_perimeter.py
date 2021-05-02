df7d86d1d2f93491c3eeb7c51d6aaf679e348eeb
Jay Calhoun <jwcalhoun2@gmail.com>
2021-05-02 22:03:38 +0000

    Updating island perimeter for pepe

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
-	if not grid:
-		return 0
-	perim = 0
-	for x in range(1, len(grid) - 1):
-		for y in range(1, len(grid[x]) - 1):
-			if grid[x][y] == 1:
-				if grid[x - 1][y] == 0:
-					perim += 1
-				if grid[x + 1][y] == 0:
-					perim += 1
-				if grid[x][y - 1] == 0:
-					perim += 1
-				if grid[x][y + 1] == 0:
-					perim += 1
-	return perim
+    if not grid:
+        return 0
+    perim = 0
+    for x in range(1, len(grid) - 1):
+        for y in range(1, len(grid[x]) - 1):
+            if grid[x][y] == 1:
+                if grid[x - 1][y] == 0:
+                    perim += 1
+                if grid[x + 1][y] == 0:
+                    perim += 1
+                if grid[x][y - 1] == 0:
+                    perim += 1
+                if grid[x][y + 1] == 0:
+                    perim += 1
+    return perim
