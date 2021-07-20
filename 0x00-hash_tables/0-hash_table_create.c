69ed5d1ee8ceaf75e0932fa079e30030723dcd14
Valinor13 <jwcalhoun2@gmail.com>
2021-07-19 20:30:37 -0500

    creating the hash tables directory

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creating an array with with a variable size
+ * @size: input size of array
+ * Return: returns pointer to malloc array or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    char *p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(size * sizeof(p));
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+return (p);
+}
---
+++ 0x00-hash_tables/README.md
@@ -0,0 +1,1 @@
+This is the hash tables project located in the Holberton low level programming repository.
