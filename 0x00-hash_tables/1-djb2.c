5c3596c6afc69fb68f0cd5f5f81bf330d0402ac2
Jay Calhoun <jwcalhoun2@gmail.com>
2021-07-24 19:11:00 +0000

    Betty fixes

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,20 +1,20 @@
 #include "hash_tables.h"
 
-/*
+/**
  * hash_djb2 - hash assigning algorithm
- * str: incoming pointer for assignment
+ * @str: incoming pointer for assignment
  * Return: int position index
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
