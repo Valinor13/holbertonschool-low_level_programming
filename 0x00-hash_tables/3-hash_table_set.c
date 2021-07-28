d65798636ec1a888499ab5b947c216cd67318be2
Jay Calhoun <jwcalhoun2@gmail.com>
2021-07-28 21:19:37 +0000

    Adding hash table set

--- 0x00-hash_tables/3-hash_table_set.c
+++ 0x00-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode;
 	unsigned long int index;
 
-	if ((!(key)) || (!(value)) || (strcmp(key, "") == 0))
+	if ((!(ht)) || (!(key)) || (!(value)) || (strcmp(key, "") == 0))
 		return (0);
 
 	newNode = malloc(sizeof(hash_node_t));
@@ -23,12 +23,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newNode->value = strdup(value);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if ((index == 0) || (ht->array[index]))
-	{
-		newNode->next = ht->array[index];
-		ht->array[index] = newNode;
-		return (1);
-	}
 	ht->array[index] = newNode;
 	return (1);
 }
