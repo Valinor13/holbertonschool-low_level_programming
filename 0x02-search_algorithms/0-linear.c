bf0dacb6ffddbf3c9ebafd046293218cba3c483e
Valinor13 <jwcalhoun2@gmail.com>
2021-09-20 16:19:48 -0500

    Update 0-linear.c

--- 0x02-search_algorithms/0-linear.c
+++ 0x02-search_algorithms/0-linear.c
@@ -10,14 +10,14 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	if (!(array))
 		return -1;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%lu] = [%d]", i, array[i]);
 		if (array[i] == value)
 			return i;
 	}
