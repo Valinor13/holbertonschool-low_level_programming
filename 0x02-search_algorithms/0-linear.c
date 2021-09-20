946946e97c10d1dc82ee7b31c248d5aade3d9762
Valinor13 <jwcalhoun2@gmail.com>
2021-09-20 16:22:25 -0500

    Adding newline to print statement

--- 0x02-search_algorithms/0-linear.c
+++ 0x02-search_algorithms/0-linear.c
@@ -13,13 +13,13 @@ int linear_search(int *array, size_t size, int value)
 	size_t i;
 
 	if (!(array))
-		return -1;
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }
