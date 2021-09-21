cce04d0b78dd2cd8717b0f0a75a60c9733e5333e
Valinor13 <jwcalhoun2@gmail.com>
2021-09-20 20:48:58 -0500

    Update 1-binary.c

--- 0x02-search_algorithms/1-binary.c
+++ 0x02-search_algorithms/1-binary.c
@@ -1,21 +1,38 @@
 #include "search_algos.h"
 
+/**
+ * print_function - prints the incoming array
+ * @array: incoming array
+ * @first: first character to print
+ * @last: last character to print
+ * Return: returns void
+ */
+
 void print_function(int *array, size_t first, size_t last)
 {
 	size_t i;
 
 	printf("Searching in array: %d", array[first]);
 
-	for (i = 0; i <= last; i++)
+	for (i = first + 1; i <= last; i++)
 	{
 		printf(", %d", array[i]);
 	}
 	putchar('\n');
 }
 
+/**
+ * recursive_function - splits the array down to 1
+ * @array: incoming array
+ * @first: first character of new split
+ * @last: last character of new split
+ * @value: value of item being searched for
+ * Return: returns index of value or -1 for failure
+ */
+
 int recursive_function(int *array, size_t first, size_t last, int value)
 {
-	size_t mid = (last / 2);
+	size_t mid = ((first + last) / 2);
 
 	print_function(array, first, last);
 
@@ -31,6 +48,14 @@ int recursive_function(int *array, size_t first, size_t last, int value)
 	else
 		return (mid);
 }
+
+/**
+ * recursive_function - splits the array down to 1
+ * @array: incoming array
+ * @size: size of array
+ * @value: value of item being searched for
+ * Return: returns index of value or -1 for failure
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
